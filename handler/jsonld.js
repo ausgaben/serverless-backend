0ebab5f780a37b205d248e3d266b3b35b66bf371
Markus Tacker <m@coderbyheart.com>
2018-11-25 23:21:19 +0100

    feat(api): add link to rename

--- handler/jsonld.js
+++ handler/jsonld.js
@@ -44,6 +44,7 @@ exports.relations = apiBase => {
   relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/monthly`), CheckingAccount.$context, false, 'update-monthly'))
   relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/currency`), CheckingAccount.$context, false, 'update-currency'))
   relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/savings`), CheckingAccount.$context, false, 'update-savings'))
+  relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/name`), CheckingAccount.$context, false, 'update-name'))
   relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/report`), Report.$context, false, 'report'))
   relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id`), Report.$context, false, 'delete'))
   // relations.addLink(CheckingAccount.$context, new Link(new URIValue(`${apiBase}/checking-account/:id/stream`), streamContext, false, 'stream'))
